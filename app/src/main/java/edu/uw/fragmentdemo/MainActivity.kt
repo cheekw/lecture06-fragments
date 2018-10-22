e5bd392f3af273df552c6d6cf59419673942424a
Victor Chee <kwc19@uw.edu>
2018-10-22 14:09:30 -0700

    frame layout

--- app/src/main/java/edu/uw/fragmentdemo/MainActivity.kt
+++ app/src/main/java/edu/uw/fragmentdemo/MainActivity.kt
@@ -2,17 +2,9 @@ package edu.uw.fragmentdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
-import org.json.JSONException
-import java.io.UnsupportedEncodingException
-import java.net.URLEncoder
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,55 +20,6 @@ class MainActivity : AppCompatActivity() {
         val text = findViewById<View>(R.id.txt_search) as EditText
         val searchTerm = text.text.toString()
 
-        downloadMovieData(searchTerm)
-    }
-
-    //download media information from iTunes
-    private fun downloadMovieData(searchTerm: String) {
-
-        var urlString = ""
-        try {
-            urlString = "https://itunes.apple.com/search?term=" + URLEncoder.encode(searchTerm, "UTF-8") + "&media=movie&entity=movie&limit=25"
-            //Log.v(TAG, urlString);
-        } catch (uee: UnsupportedEncodingException) {
-            Log.e(TAG, uee.toString())
-            return
-        }
-
-        val request = JsonObjectRequest(Request.Method.GET, urlString, null,
-                Response.Listener { response ->
-                    val movies = ArrayList<Movie>()
-
-                    try {
-                        //parse the JSON results
-                        val results = response.getJSONArray("results") //get array from "search" key
-                        for (i in 0 until results.length()) {
-                            val track = results.getJSONObject(i)
-                            if (track.getString("wrapperType") != "track")
-                            //skip non-track results
-                                continue
-                            val title = track.getString("trackName")
-                            val year = track.getString("releaseDate")
-                            val description = track.getString("longDescription")
-                            val url = track.getString("trackViewUrl")
-                            val movie = Movie(title, year, description, url)
-                            movies.add(movie)
-                        }
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                    }
-
-                    adapter!!.clear()
-                    for (movie in movies) {
-                        adapter!!.add(movie)
-                    }
-                }, Response.ErrorListener { error -> Log.e(TAG, error.toString()) })
-
-        RequestSingleton.getInstance(this).add(request)
-    }
-
-    companion object {
-
-        private val TAG = "MainActivity"
+//        downloadMovieData(searchTerm)
     }
 }
--- app/src/main/java/edu/uw/fragmentdemo/MovieListFragment.kt
+++ app/src/main/java/edu/uw/fragmentdemo/MovieListFragment.kt
@@ -10,6 +10,12 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONException
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
 import java.util.*
 
 
@@ -23,8 +29,6 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class MovieListFragment : Fragment() {
-
-    private val TAG = "ListFragment"
 
     private lateinit var adapter: ArrayAdapter<Movie>
 
@@ -44,7 +48,58 @@ class MovieListFragment : Fragment() {
             Log.v(TAG, "You clicked on: $movie")
         }
 
+//        downloadMovieData(searchTerm)
+
         return rootView
+    }
+
+    //download media information from iTunes
+    private fun downloadMovieData(searchTerm: String) {
+
+        var urlString = ""
+        try {
+            urlString = "https://itunes.apple.com/search?term=" + URLEncoder.encode(searchTerm, "UTF-8") + "&media=movie&entity=movie&limit=25"
+            //Log.v(TAG, urlString);
+        } catch (uee: UnsupportedEncodingException) {
+            Log.e(TAG, uee.toString())
+            return
+        }
+
+        val request = JsonObjectRequest(Request.Method.GET, urlString, null,
+                Response.Listener { response ->
+                    val movies = ArrayList<Movie>()
+
+                    try {
+                        //parse the JSON results
+                        val results = response.getJSONArray("results") //get array from "search" key
+                        for (i in 0 until results.length()) {
+                            val track = results.getJSONObject(i)
+                            if (track.getString("wrapperType") != "track")
+                            //skip non-track results
+                                continue
+                            val title = track.getString("trackName")
+                            val year = track.getString("releaseDate")
+                            val description = track.getString("longDescription")
+                            val url = track.getString("trackViewUrl")
+                            val movie = Movie(title, year, description, url)
+                            movies.add(movie)
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+
+                    adapter!!.clear()
+                    for (movie in movies) {
+                        adapter!!.add(movie)
+                    }
+                }, Response.ErrorListener { error -> Log.e(TAG, error.toString()) })
+
+        RequestSingleton.getInstance(context).add(request)
+    }
+
+    companion object {
+
+        private val TAG = "MainActivity"
     }
 
 
